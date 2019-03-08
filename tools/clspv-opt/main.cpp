71b191f67a4cdd516e77dc22d075821f95b459a7
Diego Novillo <dnovillo@google.com>
2019-03-08 14:51:59 -0500

    Add support for changing the location of opt and passes.
    This adds some command-line arguments to clsvp-opt to re-locate both
    LLVM opt and the clsvp pass library.
    
    Given that the tool now needs to handle its own arguments and pass
    anything it doesn't understand down to 'opt', before parsing the
    command-line, it splits the argument space in two lists: one for itself
    and another one for 'opt'.

--- tools/clspv-opt/main.cpp
+++ tools/clspv-opt/main.cpp
@@ -12,29 +12,48 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <iostream>
 #include <libgen.h>
 #include <list>
+#include <stdio.h>
 #include <string>
 #include <unistd.h>
 
+#include "llvm/Support/CommandLine.h"
+
 namespace {
 
+static llvm::cl::opt<int> clo_verbose(
+    "clo-verbose", llvm::cl::init(0),
+    llvm::cl::desc("Verbosity level.  Higher values increase verbosity."));
+
+static llvm::cl::opt<std::string>
+    clo_opt("clo-opt", llvm::cl::init(LLVM_OPT),
+            llvm::cl::desc("Path to LLVM's 'opt' tool."));
+
+static llvm::cl::opt<std::string>
+    clo_passes("clo-passes", llvm::cl::init(CLSPV_PASSES_SO),
+               llvm::cl::desc("Path to the libclspv_passes.so."));
+
 int RunOpt(int argc, const char *argv[]) {
-  const char **new_argv = new const char *[argc + 4];
+  size_t new_argc =
+      argc + 1 /* "-load" */ + 1 /* clo_passes */ + 1 /* nullptr */;
+  const char **new_argv = new const char *[new_argc];
   size_t i = 0;
-  new_argv[i++] = LLVM_OPT;
+  new_argv[i++] = clo_opt.c_str();
   new_argv[i++] = "-load";
-  new_argv[i++] = CLSPV_PASSES_SO;
+  new_argv[i++] = clo_passes.c_str();
   for (size_t j = 1; j < argc; j++) {
     new_argv[i++] = argv[j];
   }
   new_argv[i] = nullptr;
 
-  std::cout << "Executing: ";
-  for (auto i = 0; i < argc + 4; i++)
-    std::cout << new_argv[i];
-  std::cout << "\n";
+  if (clo_verbose > 0) {
+    fprintf(stderr, "Executing: ");
+    for (auto i = 0; i < new_argc - 1; i++) {
+      fprintf(stderr, "%s ", new_argv[i]);
+    }
+    fprintf(stderr, "\n");
+  }
 
   char *new_environ[] = {NULL};
   execve(new_argv[0], const_cast<char *const *>(new_argv), new_environ);
@@ -42,6 +61,51 @@ int RunOpt(int argc, const char *argv[]) {
   return -1;
 }
 
+const char **ConvertToCArray(std::vector<const char *> args) {
+  const char **argv = new const char *[args.size()];
+  size_t i = 0;
+  for (const auto &arg : args)
+    argv[i++] = arg;
+  return argv;
+}
+
 } // namespace
 
-int main(int argc, const char *argv[]) { return RunOpt(argc, argv); }
+int main(int argc, const char *argv[]) {
+  // Make sure that we only parse the command-line options registered for this
+  // app.  Any argument not recognized is passed as-is to 'opt'.
+  llvm::StringMap<llvm::cl::Option *> &options =
+      llvm::cl::getRegisteredOptions();
+
+  std::vector<const char *> args_to_keep;
+  args_to_keep.push_back(argv[0]);
+
+  std::vector<const char *> args_to_pass;
+  args_to_pass.push_back(argv[0]);
+
+  for (int i = 1; i < argc; ++i) {
+    std::string arg(argv[i]);
+    bool should_keep_arg = false;
+    for (const auto &option : options) {
+      // If this argument is something that we recognize, keep it.
+      if (arg.find(option.getKey().str()) != std::string::npos) {
+        should_keep_arg = true;
+        break;
+      }
+    }
+
+    if (should_keep_arg) {
+      args_to_keep.push_back(argv[i]);
+    } else {
+      args_to_pass.push_back(argv[i]);
+    }
+  }
+
+  llvm::cl::ParseCommandLineOptions(
+      args_to_keep.size(), ConvertToCArray(args_to_keep),
+      "Clspv wrapper tool for LLVM opt.\n\nThis tool loads the clspv passes "
+      "into opt and calls it with the given options.\nAny argument not "
+      "recognized by this tool is passed directly to opt.\n");
+
+  return RunOpt(args_to_pass.size(), ConvertToCArray(args_to_pass));
+}
