617a76dc66644236620302d554db1bf0be38cc36
Kévin Petit <kpet@free.fr>
2019-04-04 13:54:16 +0100

    Introduce a mechanism to call specific SPIR-V instructions (#309)
    Directly calling a specific SPIR-V instruction is a common
    idiom which should be shorter to write than it currently is.
    Besides, having a centralised scheme that deals with encoding
    SPIR-V intrinsics removes the need to worry about issues like
    
    This change introduces a utility function that inserts a call
    to a SPIR-V instruction passed as a parameter. It automatically
    inserts required functions into the module if needed and
    guarantees they have a unique name with a simple mangling
    scheme.
    
    The current encoding makes the IR a bit less readable as
    the functions used to encode SPIR-V instructions are named
    after the opcode number rather than the instruction name.
    This can be improved upon later if felt necessary.
    
    This change also uses the new mechanism for the mul_hi/mad_hi
    built-in functions, fixing the issue described in #308 for
    these.
    
    Signed-off-by: Kévin Petit <kpet@free.fr>

---
+++ lib/SPIRVOp.cpp
@@ -0,0 +1,85 @@
+// Copyright 2019 The Clspv Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "SPIRVOp.h"
+
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Module.h"
+
+#include "Constants.h"
+
+namespace clspv {
+
+using namespace llvm;
+
+// The mangling loosely follows the Itanium convention.
+// Its purpose is solely to ensure uniqueness of names, it is not
+// meant to convey type information.
+static std::string mangleType(Type* Ty) {
+  if (Ty->isVectorTy()) {
+    auto NumVecElems = std::to_string(Ty->getVectorNumElements());
+    return "Dv" + NumVecElems + "_" + mangleType(Ty->getScalarType());
+  } else if (Ty->isIntegerTy()) {
+    if (Ty->getScalarSizeInBits() == 8) {
+      return "h";
+    } else if (Ty->getScalarSizeInBits() == 16) {
+      return "t";
+    } else if (Ty->getScalarSizeInBits() == 32) {
+      return "j";
+    } else if (Ty->getScalarSizeInBits() == 64) {
+      return "m";
+    }
+  }
+
+  llvm_unreachable("Unhandled type in SPIR-V intrinsic name mangler");
+}
+
+Instruction* InsertSPIRVOp(Instruction* Insert, spv::Op Opcode,
+                           ArrayRef<Attribute::AttrKind> Attributes,
+                           Type* RetType, ArrayRef<Value*> Args) {
+
+  // Prepare mangled name
+  std::string MangledName = clspv::SPIRVOpIntrinsicFunction();
+  MangledName += std::to_string(Opcode);
+  MangledName += ".";
+  for (auto Arg : Args) {
+    MangledName += mangleType(Arg->getType());
+  }
+
+  // Create a function in the module
+  auto M = Insert->getModule();
+  auto Int32Ty = Type::getInt32Ty(M->getContext());
+  SmallVector<Type*, 8> ArgTypes = { Int32Ty };
+  for (auto Arg : Args) {
+    ArgTypes.push_back(Arg->getType());
+  }
+  auto NewFType = FunctionType::get(RetType, ArgTypes, false);
+  auto NewFTyC = M->getOrInsertFunction(MangledName, NewFType);
+  auto NewF = cast<Function>(NewFTyC.getCallee());
+  for (auto A : Attributes) {
+    NewF->addFnAttr(A);
+  }
+
+  // Now call it with the values we were passed
+  SmallVector<Value*, 8> ArgValues = { ConstantInt::get(Int32Ty, Opcode) };
+  for (auto Arg : Args) {
+    ArgValues.push_back(Arg);
+  }
+
+  return CallInst::Create(NewF, ArgValues, "", Insert);
+}
+
+};
---
+++ lib/SPIRVOp.h
@@ -0,0 +1,44 @@
+// Copyright 2019 The Clspv Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/IR/Attributes.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
+
+#include "spirv/1.0/spirv.hpp"
+
+namespace clspv {
+
+using namespace llvm;
+
+// Insert a call to a specific SPIR-V instruction after Insert
+//
+// A function with a name guaranteed to be unique for each combination of types
+// in Args will be used to represent the SPIR-V instruction until the
+// SPIRVProducerPass.
+//
+// The attributes passed via Attributes must all be function attributes.
+// They will be set on the function representing the SPIR-V instruction.
+//
+// Since this function may modify the symbol table of the module containing
+// Insert, it shouldn't be used while iterating over the symbols of that module
+// unless the caller knows that no new function will be created.
+Instruction* InsertSPIRVOp(Instruction* Insert, spv::Op Opcode,
+                           ArrayRef<Attribute::AttrKind> Attributes,
+                           Type* RetType, ArrayRef<Value*> Args);
+
+};
