f26dac9ae5d6fab98ca027a26e375937074770cf
alan-baker <alanbaker@google.com>
2019-11-20 14:41:51 -0500

    Update LLVM dep (#441)
    * Fix some missing includes

--- lib/ShareModuleScopeVariables.cpp
+++ lib/ShareModuleScopeVariables.cpp
@@ -20,6 +20,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "clspv/AddressSpace.h"
--- lib/SignedCompareFixupPass.cpp
+++ lib/SignedCompareFixupPass.cpp
@@ -22,6 +22,7 @@
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/raw_ostream.h"
 
 #include "clspv/Option.h"
