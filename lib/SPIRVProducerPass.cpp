9f7a9376ed9df1bb8681b3eebc8785d8a22f83b9
Alan Baker <alanbaker@google.com>
2020-05-07 15:57:43 -0400

    break formatting

--- lib/SPIRVProducerPass.cpp
+++ lib/SPIRVProducerPass.cpp
@@ -77,6 +77,7 @@ using namespace mdconst;
 namespace {
 
 cl::opt<bool> ShowResourceVars("show-rv", cl::init(false), cl::Hidden,
+
                                cl::desc("Show resource variable creation"));
 
 cl::opt<bool>
