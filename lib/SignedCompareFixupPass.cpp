616ffa2804e18513f608e83c8d3cf778353ba562
alan-baker <alanbaker@google.com>
2020-05-28 08:24:03 -0400

    Handle smax, smin and sclamp in -hack-scf (#597)
    Fixes #593
    
    * smax, smin and sclamp now get replaced when -hack-scf is specified
      * they are broken down to compare and select operations
      * the compare is then replaced like the other comparisons
    * new tests

--- lib/SignedCompareFixupPass.cpp
+++ lib/SignedCompareFixupPass.cpp
@@ -27,8 +27,10 @@
 
 #include "clspv/Option.h"
 
+#include "Builtins.h"
 #include "Passes.h"
 
+using namespace clspv;
 using namespace llvm;
 
 #define DEBUG_TYPE "signedcomparefixup"
@@ -60,6 +62,11 @@ private:
     }
     return false;
   }
+
+  // Replaces |call| which is a smin, smax or sclamp call with an equivalent
+  // instruction stream. Also adds the comparisons introduced to |work_list|.
+  void ReplaceBuiltin(CallInst *call, Builtins::BuiltinType type,
+                      SmallVectorImpl<ICmpInst *> *work_list);
 };
 } // namespace
 
@@ -80,6 +87,7 @@ bool SignedCompareFixupPass::runOnModule(Module &M) {
     return Changed;
   }
 
+  SmallVector<Instruction *, 16> to_remove;
   SmallVector<ICmpInst *, 16> work_list;
   for (auto &F : M) {
     for (auto &BB : F) {
@@ -87,6 +95,30 @@ bool SignedCompareFixupPass::runOnModule(Module &M) {
         if (auto *icmp = dyn_cast<ICmpInst>(&inst)) {
           if (IsSignedRelational(icmp->getPredicate())) {
             work_list.push_back(icmp);
+          }
+        } else if (auto call = dyn_cast<CallInst>(&inst)) {
+          // Same bug exists for smin, smax and sclamp. Break those calls down
+          // and then replace the resulting comparisons too.
+          auto &info = Builtins::Lookup(call->getCalledFunction());
+          switch (info.getType()) {
+          case Builtins::kMax:
+          case Builtins::kMin:
+          case Builtins::kClamp: {
+            auto param_info = info.getParameter(0);
+            // Note that this also covers vector operands.
+            if (param_info.is_signed &&
+                param_info.type_id == llvm::Type::IntegerTyID) {
+              if (ShowSCF) {
+                outs() << "SCF: Replace " << *call << "\n";
+              }
+              ReplaceBuiltin(call, info.getType(), &work_list);
+              Changed = true;
+              to_remove.push_back(call);
+            }
+            break;
+          }
+          default:
+            break;
           }
         }
       }
@@ -189,6 +221,9 @@ bool SignedCompareFixupPass::runOnModule(Module &M) {
     icmp->replaceAllUsesWith(replacement);
   }
 
+  for (auto *inst : to_remove) {
+    inst->eraseFromParent();
+  }
   for (auto *inst : work_list) {
     inst->eraseFromParent();
   }
@@ -199,4 +234,41 @@ bool SignedCompareFixupPass::runOnModule(Module &M) {
 
   return Changed;
 }
+
+void SignedCompareFixupPass::ReplaceBuiltin(
+    CallInst *call, Builtins::BuiltinType type,
+    SmallVectorImpl<ICmpInst *> *work_list) {
+  // The returns from CreateICmpSLT are tested because they may have been
+  // folded to a constant.
+  IRBuilder<> builder(call->getContext());
+  builder.SetInsertPoint(call);
+  Value *out = nullptr;
+  if (type == Builtins::kClamp) {
+    // clamp(x,b,c) is min(max(x, b), c). Results are undefined if b > c, so
+    // both comparisons can be made against x.
+    auto x = call->getOperand(0);
+    auto min = call->getOperand(1);
+    auto max = call->getOperand(2);
+    auto cmp1 = builder.CreateICmpSLT(x, min);
+    auto sel = builder.CreateSelect(cmp1, min, x);
+    auto cmp2 = builder.CreateICmpSLT(max, x);
+    out = builder.CreateSelect(cmp2, max, sel);
+    if (auto icmp = dyn_cast<ICmpInst>(cmp1))
+      work_list->push_back(icmp);
+    if (auto icmp = dyn_cast<ICmpInst>(cmp2))
+      work_list->push_back(icmp);
+  } else {
+    bool is_min = type == Builtins::kMin;
+    auto lhs = call->getOperand(0);
+    auto rhs = call->getOperand(1);
+    if (is_min)
+      std::swap(lhs, rhs);
+    auto cmp = builder.CreateICmpSLT(lhs, rhs);
+    out = builder.CreateSelect(cmp, call->getOperand(1), call->getOperand(0));
+    if (auto icmp = dyn_cast<ICmpInst>(cmp))
+      work_list->push_back(icmp);
+  }
+  call->replaceAllUsesWith(out);
+}
+
 } // namespace
