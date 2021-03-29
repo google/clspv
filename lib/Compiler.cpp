7130aa5d90bb41ac98cde2eb151a6a2bd2751a88
Kévin Petit <kevin.petit@arm.com>
2021-03-29 21:13:56 +0100

    do not disable rw images feature macro

--- lib/Compiler.cpp
+++ lib/Compiler.cpp
@@ -403,7 +403,6 @@ clang::TargetInfo *PrepareTargetInfo(CompilerInstance &instance) {
     Opts["__opencl_c_generic_address_space"] = false;
     Opts["__opencl_c_atomic_order_seq_cst"] = false;
     Opts["__opencl_c_device_enqueue"] = false;
-    Opts["__opencl_c_read_write_images"] = false;
     Opts["__opencl_c_program_scope_global_variables"] = false;
 
     if (!clspv::Option::ImageSupport()) {
