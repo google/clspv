5a9efef59f7f2ed03fc2e6aac2ec3fb06152d3e3
alan-baker <alanbaker@google.com>
2022-04-26 21:29:32 -0400

    Fix some includes in updated passes (#842)

--- lib/SignedCompareFixupPass.h
+++ lib/SignedCompareFixupPass.h
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/PassManager.h"
 
