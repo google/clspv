c9ca692600e9a83841e5e6a4f2007b44ec2b0b1a
Romaric Jodin <89833130+rjodinchr@users.noreply.github.com>
2023-09-28 19:16:12 +0200

    fix LowerPrivatePointerPHIPass (#1242)
    Use proper type for ptr offset

--- lib/LowerPrivatePointerPHIPass.cpp
+++ lib/LowerPrivatePointerPHIPass.cpp
@@ -219,10 +219,13 @@ void clspv::LowerPrivatePointerPHIPass::runOnFunction(Function &F) {
         IRBuilder<> B(icmp);
         if (intToPtr) {
           icmp->setOperand(otherOpIsIntToPtr, intToPtr->getOperand(0));
+          Type *PtrOffTy = clspv::PointersAre64Bit(*F.getParent())
+                               ? B.getInt64Ty()
+                               : B.getInt32Ty();
           if (DynVal == nullptr) {
-            DynVal = B.getInt32(CstVal);
+            DynVal = ConstantInt::get(PtrOffTy, CstVal);
           } else if (CstVal != 0) {
-            DynVal = B.CreateAdd(DynVal, B.getInt32(CstVal));
+            DynVal = B.CreateAdd(DynVal, ConstantInt::get(PtrOffTy, CstVal));
           }
           icmp->setOperand(opId, DynVal);
           if (intToPtr->getNumUses() == 0) {
