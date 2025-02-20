0e34c4db0b53154621d3a9e9333fd9973845820f
David Neto <dneto@google.com>
2025-02-20 01:41:35 -0500

    kokoro: check-format uses a streamlined formatter image (#1454)
    Avoid downloading all of LLVM and Clang source during check-format.
    
    Instead, take clang-format-diff.py from the image.
    
    Update ./utils/check_code_format.sh so it can use env vars:
     - CLANG_FORMAT to find clang-format
     - CLANG_FORMAT_DIFF to find clang-format-diff.py
    
    Both of these are set in the Docker image.
    
    BUG=397439901

--- utils/check_code_format.sh
+++ utils/check_code_format.sh
@@ -12,11 +12,29 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# Script to determine if source code in Pull Request is properly formatted.
+
+
+# This script determine if the source code in a Pull Request is properly formatted.
 # Exits with non 0 exit code if formatting is needed.
-#
-# This script assumes to be invoked at the project root directory.
+# Assumptions:
+#    - git and python3 are on the path
+#    - Runs from the project root diretory.
+#    - 'clang-format' is on the path, or env var CLANG_FORMAT points to it.
+#    - 'clang-format-diff.py' is in the utils directory, or env var
+#       points to it.CLANG_FORMAT_DIFF
+
+CLANG_FORMAT=${CLANG_FORMAT_DIFF:-clang-format}
+if [ ! -f "$CLANG_FORMAT" ]; then
+  echo missing clang-format: set CLANG_FORMAT or put clang-format in the PATH
+  exit 1
+fi
+
+# Find clang-format-diff.py from an environment variable, or use a default
+CLANG_FORMAT_DIFF=${CLANG_FORMAT_DIFF:-./utils/clang-format-diff.py}
+if [ ! -f "$CLANG_FORMAT_DIFF" ]; then
+  echo missing clang-format-diffy.py: set CLANG_FORMAT_DIFF or put it in ./utils/clang-format-diff.py
+  exit 1
+fi
 
 if [ "$1" = "FULL" ]; then
   FILES_TO_CHECK=$(git diff --name-only HEAD~ | grep -E ".*\.(cpp|cc|c\+\+|cxx|c|h|hpp)$")
@@ -30,9 +48,9 @@ if [ -z "${FILES_TO_CHECK}" ]; then
 fi
 
 if [ "$1" = "FULL" ]; then
-  FORMAT_DIFF=$(git diff -U0 HEAD~ -- ${FILES_TO_CHECK} | python3 ./utils/clang-format-diff.py -p1 -style=file)
+  FORMAT_DIFF=$(git diff -U0 HEAD~ -- ${FILES_TO_CHECK} | python3 "$CLANG_FORMAT_DIFF" -p1 -style=file -binary="$CLANG_FORMAT" )
 else
-  FORMAT_DIFF=$(git diff -U0 main -- ${FILES_TO_CHECK} | python3 ./utils/clang-format-diff.py -p1 -style=file)
+  FORMAT_DIFF=$(git diff -U0 main -- ${FILES_TO_CHECK} | python3 "$CLANG_FORMAT_DIFF" -p1 -style=file -binary="$CLANG_FORMAT" )
 fi
 
 if [ -z "${FORMAT_DIFF}" ]; then
