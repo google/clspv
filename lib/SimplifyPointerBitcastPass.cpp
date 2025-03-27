5104dc297b8a0072bdba59e81e8561f9785874a1
Austin Annestrand <a.annestrand@samsung.com>
2025-03-27 15:50:35 -0500

    Fix SimplifyPointerBitcastPass hang on const GEP GVs
    This fixes issue where SimplifyPointerBitcastPass endlessly
    loops on converting and reconverting these particular GEPs.

--- lib/SimplifyPointerBitcastPass.cpp
+++ lib/SimplifyPointerBitcastPass.cpp
@@ -572,6 +572,13 @@ bool clspv::SimplifyPointerBitcastPass::runOnImplicitGEP(Module &M) const {
     LLVM_DEBUG(dbgs() << "\n##runOnImplicitGEP (from GV):\nreplacing: ";
                gep->dump());
     LLVM_DEBUG(dbgs() << "by: "; new_gep->dump(););
+
+    // Tag all-constant-indice-global-GEPs to stay in normalized form to
+    // prevent conversion-thrashing with latter sub-passes
+    // TODO: this could be better handled without the use of metadata tagging
+    MDNode *MD = MDNode::get(new_gep->getContext(), {});
+    new_gep->setMetadata("clspv.const_gep_gv", MD);
+
     gep->replaceAllUsesWith(new_gep);
     gep->eraseFromParent();
     changed = true;
@@ -787,6 +794,9 @@ bool clspv::SimplifyPointerBitcastPass::runOnUpgradeableConstantCasts(
               IsClspvResourceOrLocal(gep->getPointerOperand())) {
             continue;
           }
+          if (gep->getMetadata("clspv.const_gep_gv"))
+            continue;
+
           uint64_t cstVal;
           Value *dynVal;
           size_t smallerBitWidths;
@@ -983,6 +993,8 @@ bool clspv::SimplifyPointerBitcastPass::runOnUnneededIndices(Module &M) const {
         }
         if (auto gep = dyn_cast<GetElementPtrInst>(source)) {
           if (gep->getNumIndices() <= 1)
+            continue;
+          if (gep->getMetadata("clspv.const_gep_gv"))
             continue;
           if (SizeInBits(DL, source_ty) < SizeInBits(DL, dest_ty)) {
             if (auto cst = dyn_cast<ConstantInt>(
