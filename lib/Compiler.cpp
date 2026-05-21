aab627d4a104dfc3061b0f02a476c70869985532
Romaric Jodin <rjodin@google.com>
2026-05-21 15:24:24 +0200

    bring back legacy option for --arch

--- lib/Compiler.cpp
+++ lib/Compiler.cpp
@@ -182,7 +182,10 @@ static llvm::cl::opt<SPIRVArch> target_arch(
         clEnumValN(SPIRVArch::SPIRV32, "spirv32",
                    "spirv32-unknown-vulkan target (pointers are 32-bit)"),
         clEnumValN(SPIRVArch::SPIRV64, "spirv64",
-                   "spirv64-unknown-vulkan target (pointers are 64-bit)")));
+                   "spirv64-unknown-vulkan target (pointers are 64-bit)"),
+        // Legacy options: kept for compatibility, omitted from standard help text
+        clEnumValN(SPIRVArch::SPIRV32, "spir", ""),
+        clEnumValN(SPIRVArch::SPIRV64, "spir64", "")));
 
 namespace {
 struct OpenCLBuiltinMemoryBuffer final : public llvm::MemoryBuffer {
